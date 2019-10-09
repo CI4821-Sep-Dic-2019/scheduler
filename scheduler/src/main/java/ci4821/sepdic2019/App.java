4566eb202ffec584b89ae0ca36691da49326df19
Gustavo Castellanos <gustavoaca1997@gmail.com>
2019-10-09 17:54:20 -0400

    Update App.java

--- scheduler/src/main/java/ci4821/sepdic2019/App.java
+++ scheduler/src/main/java/ci4821/sepdic2019/App.java
@@ -1,15 +1,4 @@
 package ci4821.sepdic2019;
-
-// import java.util.Arrays;
-// import java.util.Iterator;
-// import java.time.format.DateTimeFormatter;  
-// import java.time.LocalDateTime;    
-
-// import ci4821.sepdic2019.ds.Log;
-// import ci4821.sepdic2019.system.CPU;
-// import ci4821.sepdic2019.system.Resource;
-// import ci4821.sepdic2019.system.Process;
-// import ci4821.sepdic2019.utils.Parser;
 import ci4821.sepdic2019.simulator.Simulator;
 
 /**
